e933d6add759093e6011b8682eb9621abe42748e
Siilarsi Lorenzen <siilarsi@gmail.com>
2025-10-21 10:41:56 +0200

    Refine section spacing scale

---
+++ static/chunks/app/insights/[slug]/page-e7617326fce027b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[967,301,45,341,931,330],{5918:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=5918)}),_N_E=n.O()}]);
