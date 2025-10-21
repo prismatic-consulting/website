43d1e5ca60d7c9bcd07f1f0110f430abf07a6042
Siilarsi Lorenzen <siilarsi@gmail.com>
2025-10-21 17:15:12 +0200

    Ensure flattened static assets remain available at /_next/static

--- static/chunks/app/insights/page-3d18b399c86c89cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[341,301,45,931,967,330],{5918:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=5918)}),_N_E=n.O()}]);
